c6e16ab3e40d869777134b9545cf24e4f5a9782d
Anthony <quereantho@gmail.com>
2021-05-21 15:38:43 +0200

    Add free reader

--- src/parser/reader/reader.c
+++ src/parser/reader/reader.c
@@ -1,11 +1,19 @@
 #include "reader.h"
 
+/**
+ * Read the next character of a file. It will be store in the reader under `currentChar`
+ * \param cursor cursor used to read the file
+ */
 void nextCharacter(reader cursor)
 {
     cursor->currentChar = fgetc(cursor->file);
     printf("\e[0;34m%c\e[0;37m", cursor->currentChar);
 }
 
+/**
+ * Create a reader from the filename. /!\ Remember to close the file after using it
+ * \param filemane path to the file to read
+ */
 reader createReader(char *filename)
 {
     reader cursor = (reader)malloc(sizeof(t_reader));
@@ -15,4 +23,14 @@ reader createReader(char *filename)
     nextCharacter(cursor);
 
     return cursor;
+}
+
+/**
+ * Free the memory of the given reader
+ * \param cursor reader to free
+ */
+void freeReader(reader cursor)
+{
+    fclose(cursor->file);
+    free(cursor);
 }
