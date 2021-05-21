c44e26e49914d3246be6498fad903486ccf81f25
Anthony <quereantho@gmail.com>
2021-05-21 15:28:58 +0200

    Start documenting parser.c

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -4,34 +4,54 @@
 #include "../tag/tag.h"
 #include "reader/reader.h"
 
-
+/**
+ * Defines a parser.
+ * A parser is a structure composed of 2 functions :
+ * - `execute` => a function that parse a a texte using a `reader`and returning a tag
+ * - `verify` => a function that takes a `char*` represeting the current tag as a parameter and return true if it is its tag
+ */
 typedef struct s_parser
 {
     a_tag  (*execute)(reader);
     int (*verify)(char*);
 } t_parser;
 
+/**
+ * Check if the current tag is equal to the secound parameter. If it is not, thorw an error and exit the programm 
+ * \param cursor cursor of the current file with a current tag
+ * \param tagName expected name of the tag
+*/
 void assertCurrentTag(reader cursor, char* tagName);
 
-
-a_tag  parser(char* filename);
-
-
-t_tag readTag(reader cursor, t_tagName name);
+/**
+ * Parse a filename and return a a_tag struct
+ * \param filemane path to the file to parse
+ */
+a_tag parser(char* filename);
 
 /**
- * TODO : words with more than 256 characters
+ * Return 1 if the given character is a space char (' ', '\t', '\n', EOF)
+ * \param c character to control
  */
-
-a_tag  readText(reader cursor);
-
 int estEspace(char c);
 
+/**
+ * Read the file until a non space character is found
+ * \param cursor Cursor used to read the file
+ */
 void readSpaces(reader cursor);
 
+/**
+ * Return 1 if the character is a '<'
+ * \param c the character to control
+ */
 int estChevronGauche(char c);
 
-a_tag  lireMotSimple(reader cursor);
+/**
+ * Read a motSimple and return the corresponding tag.
+ * \param reader reader used to parse the file
+ */
+a_tag lireMotSimple(reader cursor);
 
 /**
  * \brief read an important word in the file (it reads "<important> + content + </important>")
@@ -44,13 +64,30 @@ a_tag  lireMotSimple(reader cursor);
  * <important> exemple </important>
  * (spaces are facultative)
  */
-a_tag  lireMotImportant(reader cursor);
+a_tag lireMotImportant(reader cursor);
 
+/**
+ * Try to use a parser zero or multiple times. Add the parsed tags to a tagList and return it.
+ * \param parser parser that needed 
+ * \param cursor reader used to read the file
+ */
 tagList zeroOuPlus(t_parser parser, reader cursor);
 
+/**
+ * Function that takes an array of parsers as argument and try to execute them. 
+ * Return the parsed tag or NULL if no parser worked
+ * 
+ * \param cursor cursor used to read the file
+ * \param parsers array of parsers
+ * \param nbParsers number of parsers in the array
+ */
 a_tag ou(reader cursor, t_parser *parsers, int nbParsers);
 
-
+/**
+ * Compare 2 char* char by char. If they are the same return 1 els, return 0
+ * \param str1 first char* to compare
+ * \param str2 second char* to compare
+ */ 
 int compareStr(char* str1, char* str2);
 
 
